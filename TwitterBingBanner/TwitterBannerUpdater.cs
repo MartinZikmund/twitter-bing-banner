0ff15c047e784d0ac19ebc216b7ae121cb80bdac
Martin Zikmund <martin@zikmund.dev>
2021-05-22 20:43:02 +0200

    Using LinqToTwitter and ComputerVision

--- TwitterBingBanner/TwitterBannerUpdater.cs
+++ TwitterBingBanner/TwitterBannerUpdater.cs
@@ -1,4 +1,8 @@
 using System;
+using System.IO;
+using System.Threading.Tasks;
+using LinqToTwitter;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -8,8 +12,19 @@ namespace TwitterBingBanner
     public static class TwitterBannerUpdater
     {
         [FunctionName("TwitterBannerUpdater")]
-        public static void Run([TimerTrigger("0 0 0 * * *")]TimerInfo myTimer, ILogger log)
+        public static async Task Run([TimerTrigger("0 0 0 * * *")] TimerInfo timer, ILogger log)
         {
+
+            string bingImageUrl = "?";
+            var client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(""));
+            using var imageStream = await client.GenerateThumbnailAsync(1500, 500, bingImageUrl, smartCropping: true);
+            using var streamReader = new MemoryStream();
+            imageStream.CopyTo(streamReader);
+            var imageData = streamReader.ToArray();
+            var twitterCtx = new TwitterContext();
+            await twitterCtx.UpdateProfileBannerAsync(imageData, 1500, 500, 0, 0);
+
+
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
         }
     }
